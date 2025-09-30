e767a3d4a7cc098368d991900c9956074c05f3a6
Rosano Coutinho <github.com@xeari.com>
2025-09-30 16:47:40 +0100

    trigger app load from remoteStorage.ready

--- rs-app.js
+++ rs-app.js
@@ -11,4 +11,9 @@ remoteStorage.todos.cacheTodos();
 // Setup after page loads
 document.addEventListener('DOMContentLoaded', () => {
   (new Widget(remoteStorage)).attach(document.body.insertBefore(document.createElement('widget-container'), document.querySelector('.todoapp')));
+
+  remoteStorage.on('ready', () => {
+    // trigger app setup from `app.js`
+    todo.controller.setView(document.location.hash);
+  });
 });
