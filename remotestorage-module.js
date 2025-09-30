372de6108d4a6f0510a83960d4a7d0549591eb62
Rosano Coutinho <github.com@xeari.com>
2025-09-30 16:45:17 +0100

    format objects for expected schema

--- remotestorage-module.js
+++ remotestorage-module.js
@@ -9,19 +9,40 @@ const todos = {
       required: ['description'],
     });
 
+    const inject = function (object, properties) {
+      return Object.assign(Object.assign({}, object), properties);
+    };
+
+    const formatOut = function (object, properties) {
+      object.description = object.title;
+      delete object.title;
+
+      return inject(object, properties);
+    };
+
+    const formatIn = function (object, properties) {
+      object.title = object.description;
+      delete object.description;
+      
+      return Object.assign(Object.assign({}, object), properties);
+    };
+
     return {
       exports: {
         cacheTodos: () => privateClient.cache(''),
 
         on: privateClient.on,
 
-        addTodo: (description) => privateClient.storeObject('todo', `${ new Date().getTime() }`, { description }),
+        addTodo: (object) => {
+          id = `${ new Date().getTime() }`;
+          return privateClient.storeObject('todo', id, formatOut(object, { id }))
+        },
 
-        updateTodo: (id, description) => privateClient.storeObject('todo', id, { description }),
+        updateTodo: (id, object) => privateClient.storeObject('todo', id, formatOut(object, { id })),
 
         removeTodo: privateClient.remove.bind(privateClient),
 
-        getAllTodos: () => privateClient.getAll('', false),
+        getAllTodos: () => privateClient.getAll('', false).then(map => Object.entries(map).reduce((coll, item) => coll.concat(formatIn(item[1], { id: item[0] })), [])),
       }
     }
   }
